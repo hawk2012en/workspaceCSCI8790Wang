bd04e38158aee7708ed1253b329586a59566d840
jwang12 <jwang12@chem-its.unl.edu>
2019-03-14 22:09:54 -0500

    Update javassist-project-Wang-0221

--- javassist-project-Wang-0221/srcfiles/ComponentApp.java
+++ javassist-project-Wang-0221/srcfiles/ComponentApp.java
@@ -1,26 +1,12 @@
 package target;
 
-@Table(name = "COMPONENT_APP", id = 101)
-@Author(name = "CA", year = 1991)
 public class ComponentApp {
-
-   @Column(name = "COMPONENT_NAME", id = 102)
-   @Author(name = "CB", year = 1992)
-   String componentName;
-
-   @Author(name = "CD", year = 1993)
-   String componentLocation;
-
-   @Column(name = "COMPONENT_PROVIDER", id = 104)
-   @Author(name = "CC", year = 1994)
-   String componentProvider;
-
-   public void getComponent(int x, int y, int z) {
-      System.out.println("[DBG] getComponent called");
+   public void foo(int x, int y, int z) {
+      System.out.println("[DBG] foo called");
    }
 
    public static void main(String[] args) {
-      ComponentApp app = new ComponentApp();
-      app.getComponent(1, 2, 3);
+      ComponentApp f = new ComponentApp();
+      f.foo(1, 2, 3);
    }
 }
--- javassist-project-Wang-0221/srcfiles/ServiceApp.java
+++ javassist-project-Wang-0221/srcfiles/ServiceApp.java
@@ -1,26 +1,12 @@
 package target;
 
-@Table(name = "SERVICE_APP", id = 201)
-@Author(name = "SA", year = 2001)
 public class ServiceApp {
-
-   @Row(name = "SERVICE_NAME", id = 202)
-   @Author(name = "SB", year = 2002)
-   String serviceName;
-   
-   @Author(name = "SD", year = 2003)
-   String serviceLocation;
-
-   @Row(name = "SERVICE_PROVIDER", id = 204)
-   @Author(name = "SC", year = 2004)
-   String serviceProvider;
-
-   public void getService(int p1, int p2, int p3) {
-      System.out.println("[DBG] getService called");
+   public void bar(int p1, int p2, int p3) {
+      System.out.println("[DBG] bar called");
    }
 
    public static void main(String[] args) {
-      ServiceApp app = new ServiceApp();
-      app.getService(10, 20, 30);
+      ServiceApp f = new ServiceApp();
+      f.bar(10, 20, 30);
    }
 }
